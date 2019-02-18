a8c62ce264a2c994e0a790db5fd2a54c1a36972d
MininMobile <notminin@pm.me>
2019-02-18 18:11:35 +0000

    add rooms list and get room members

--- src/manex.js
+++ src/manex.js
@@ -2,13 +2,23 @@ const matrix = require("matrix-js-sdk");
 
 const client = matrix.createClient("https://matrix.org");
 
-let loginForm = document.getElementById("login-form");
-let loginUsername = document.getElementById("login-username");
-let loginPassword = document.getElementById("login-password");
-
 let loadingSplash = document.getElementById("loading-splash");
 let loginWrapper = document.getElementById("login-wrapper");
 let chatWrapper = document.getElementById("chat-wrapper");
+
+let roomList = document.getElementById("room-list")
+let messageList = document.getElementById("message-list")
+let memberList = document.getElementById("member-list")
+
+let rooms = undefined;
+
+let currentRoom = undefined;
+
+// add login
+
+let loginForm = document.getElementById("login-form");
+let loginUsername = document.getElementById("login-username");
+let loginPassword = document.getElementById("login-password");
 
 setTimeout(() => {
 	if (window.localStorage.username && window.localStorage.password) {
@@ -20,13 +30,13 @@ setTimeout(() => {
 	}
 }, 100);
 
-{ // login
-	form.addEventListener("submit", () => {
-		client.loginWithPassword(loginUsername.value, loginPassword.value)
-			.then((data) => startClient(data, true))
-			.catch((e) => console.error(e));
-	});
-}
+loginForm.addEventListener("submit", () => {
+	client.loginWithPassword(loginUsername.value, loginPassword.value)
+		.then((data) => startClient(data, true))
+		.catch((e) => console.error(e));
+});
+
+// actions
 
 function startClient(data, fresh = false) {
 	if (!window.localStorage.username && !window.localStorage.password && fresh) {
@@ -38,13 +48,60 @@ function startClient(data, fresh = false) {
 		loginWrapper.classList.add("disabled");
 		chatWrapper.classList.remove("disabled");
 
-		hideLoading();
+		client.startClient();
+
+		client.once("sync", (state, prevState, data) => {
+			if (state == "PREPARED") {
+				getRooms();
+				hideLoading();
+			}
+		});
 	});
+}
+
+function getRooms() {
+	roomList.innerHTML = "";
+
+	rooms = client.getRooms();
+
+	rooms.forEach((r) => {
+		let button = document.createElement("div");
+			button.classList.add("room");
+			button.innerText = r.name;
+			button.addEventListener("click", () => {
+				openRoom(r);
+			});
+			roomList.appendChild(button);
+	});
+}
+
+function openRoom(room) {
+	currentRoom = room;
+
+	{ // update member list
+		memberList.innerHTML = "";
+
+		room.getJoinedMembers().forEach((m) => {
+			let button = document.createElement("div");
+				button.classList.add("room");
+				button.innerText = m.name;
+				button.addEventListener("click", () => {
+					// smth
+				});
+				memberList.appendChild(button);
+		});
+	}
+
+	{ // get timeline
+		// smth
+	}
 }
 
 function logout() {
 	window.localStorage.removeItem("password");
 	window.localStorage.removeItem("username");
+
+	client.stopClient();
 
 	window.location.reload();
 }
